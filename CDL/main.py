107917ce5b2dc1590363a3febdc9e0286200bcb9
mtigwell <mtigwell@gatewaysoftware.ca>
2021-05-11 11:45:38 -0400

    new results

--- CDL/main.py
+++ CDL/main.py
@@ -19,11 +19,21 @@ except:
         rating_matrix = pickle.load(handle2)
 
 
-for lamU in [0.01, 0.1, 1, 10]:
-    for lamV in [0.1, 1, 10, 100]:
-        for lamW in [0.1, 1, 10]:
-            for lv in [0.0001, 0.001, 0.01, 0.1, 1]:
-                result_directory = 'results/U{}V{}W{}LV{}'.format(lamU, lamV, lamW, lv)                
-                cdl = CDL(rating_matrix, item_matrix, lambda_u=lamU, lambda_v=lamV, lambda_w=lamW, lv=lv, K=50, epochs=15, batch=256, dir_save=result_directory, dropout=0.1, recall_m=100)
-                cdl.build_model()
-                cdl.training(rating_matrix)
+# lambda hyperparameter search
+# for lamU in [0.1, 1, 10]:
+#     for lamV in [1, 10, 100]:
+#         for lamW in [0.1, 1, 10]:
+#             for lv in [0.001, 0.01, 0.1]:
+#                 result_directory = 'results/U{}V{}W{}LV{}'.format(lamU, lamV, lamW, lv)                
+#                 cdl = CDL(rating_matrix, item_matrix, lambda_u=lamU, lambda_v=lamV, lambda_w=lamW, lv=lv, K=50, epochs=15, batch=256, dir_save=result_directory, dropout=0.1, recall_m=100)
+#                 cdl.build_model()
+#                 cdl.training(rating_matrix)
+
+
+for dropout in [0.05, 0.1, 0.2]:
+    for K in [50]:
+        for batch in [32, 256, 1024]:
+            result_directory = 'results/drop{}K{}batch{}'.format(dropout, K, batch)                
+            cdl = CDL(rating_matrix, item_matrix, lambda_u=1, lambda_v=10, lambda_w=10, lv=0.01, K=K, epochs=15, batch=batch, dir_save=result_directory, dropout=dropout, recall_m=100)
+            cdl.build_model()
+            cdl.training(rating_matrix)
